c21ab0d5d46ff95bdf07cbe6eae2fb2ae2800cb6
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-02-21 01:13:37 +0900

    Add:リクエストを受けられるようにする

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("createUser 関数実行")
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("getUser 関数実行")
+}
 
 func main() {
     router := mux.NewRouter()
 
     router.HandleFunc("/user/create", createUser).Methods("POST")
     router.HandleFunc("/user/get", getUser).Methods("POST")
+
+		log.Println("サーバー起動 : 8080 port で受信")
+
+    log.Fatal(http.ListenAndServe(":8080", router))
 }
